f36805bf3a046212f1a497342733812dcb2b7988
Jesus Garcia <jgarciajr45@gmail.com>
2024-06-05 08:45:48 -0700

    Exercise 2

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -5,6 +5,8 @@
         static void Main(string[] args)
         {
 
+            //EXERCISE 1
+
             int favNum = 5;
             Console.WriteLine("Guess my favorite number.");
 
@@ -24,9 +26,36 @@
             }
 
 
+            //EXERCISE 2
+
+            Console.WriteLine("What is your favorite school subject?");
+            String favSub = Console.ReadLine();
+
+            switch (favSub)
+            {
+                case "Math":
+                    Console.WriteLine("Math? Nerd.");
+                    break;
+                case "Science":
+                    Console.WriteLine("Science? Nerd");
+                    break;
+                case "History":
+                    Console.WriteLine("History? Nerd.");
+                    break;
+                case "English":
+                    Console.WriteLine("English? Nerd.");
+                    break;
+                case "PE":
+                    Console.WriteLine("PE? Nerd.");
+                    break;
+                default:
+                    Console.WriteLine("Interesting. Nerd.");
+                    break;
+
+            }
 
 
-            
+
         }
     }
 }
