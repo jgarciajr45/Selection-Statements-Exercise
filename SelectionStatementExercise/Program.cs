53f7503a09cc57b5b31f3fd7c48b36b6fb81047b
Jesus Garcia <jgarciajr45@gmail.com>
2024-06-05 08:09:05 -0700

    Exercise 1

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            int favNum = 5;
+            Console.WriteLine("Guess my favorite number.");
+
+            var userInput = int.Parse(Console.ReadLine());
+
+            if(userInput < favNum)
+            {
+                Console.WriteLine("Too low.");
+            }
+            else if(userInput > favNum)
+            {
+                Console.WriteLine("Too High");
+            }
+            else
+            {
+                Console.WriteLine("Nevermind");
+            }
+
+
+
+
+            
         }
     }
 }
